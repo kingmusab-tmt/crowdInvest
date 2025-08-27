13dcbe5d82b6482c160acf7555f01bba1549a408
Musab Mubaraq MBURAIMOH <musab.buraimoh@gmail.com>
2025-08-27 15:34:26 +0000

    how can i copy the prototype to my local machine

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,14 +6,14 @@ import { getAuth, Auth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "crowd-invest-835s9",
-  "appId": "1:1015186708913:web:350bf70c39bdbd1085c590",
-  "storageBucket": "crowd-invest-835s9.firebasestorage.app",
-  "apiKey": "AIzaSyB7uWtDRa741techaT0lQ7fuMOJH3cp2qM",
-  "authDomain": "crowd-invest-835s9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1015186708913"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
